a3aa3b149dbc90e33b6de00ba3ea82371a59e3e0
Panteleimon Pantelidis <58360604+ppantele@users.noreply.github.com>
2024-08-10 14:23:40 +0300

    Update DeceptionECG_Start.py

--- DeceptionECG_Start.py
+++ DeceptionECG_Start.py
@@ -2,4 +2,4 @@ import requests
 url = 'https://raw.githubusercontent.com/ppantele/DeceptionECG/main/DeceptionECG_Functions.py'
 r = requests.get(url)
 exec(r.content)
-print('DeceptionECG tools loaded. Use: \n  1. DeceptionECG_GenPurp(n), n: Number of samples \n      --> To generate new ECG samples. [Output: n*12*1000 nd.array]\n  2. DeceptionECG_DiseaseSpec(input, disease), input: Input signal {n*12*1000 nd.array}, disease: Disease type {"AFIB" or "AMI" or "WPW"} \n      --> To generate new ECG samples. [Output: n*12*1000 nd.array]')
+print('DeceptionECG tools loaded. Use: \n  1. DeceptionECG_GenPurp(n), n: Number of samples \n      --> To generate new ECG samples. [Output: n*12*1000 nd.array]\n  2. DeceptionECG_DiseaseSpec(input, disease), input: Input signal {n*12*1000 nd.array}, disease: Disease type {"AFIB" or "AMI" or "WPW"} \n      --> To generate new ECG samples. [Output: n*12*1000 nd.array]\n  3. DeceptionECG_SignalPlot(input), input: Input signal {n*12*1000 nd.array} \n      --> To plot an ECG sample. [Output: Within IDE figure]')
